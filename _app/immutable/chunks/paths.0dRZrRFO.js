d97c95358642acf0ba2d91f4bb753ef2f639bd37
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-04-22 21:43:50 +0200

    static full test

--- _app/immutable/chunks/paths.0dRZrRFO.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_154a90w)==null?void 0:s.base)??"";var a;(a=globalThis.__sveltekit_154a90w)==null||a.assets;export{e as b};
---
+++ _app/immutable/entry/start.BtaO7Xly.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.7w3lIAxq.js";export{t as start};
