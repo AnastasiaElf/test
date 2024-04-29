e5a733d9c8d7bb4484572e18d6dca3a080b80109
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-04-29 15:47:34 +0200

    build with store

--- _app/immutable/chunks/paths.D6ikm_Zd.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1poqf0e)==null?void 0:s.base)??"/ocs";var e;const t=((e=globalThis.__sveltekit_1poqf0e)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.CLQtE2Rt.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D3OQ6B7E.js";export{t as start};
