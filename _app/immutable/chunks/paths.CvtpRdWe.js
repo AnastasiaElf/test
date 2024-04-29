56cd4c2ff0a7d91f3f6b0cb079628de8e4724434
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-04-29 16:04:54 +0200

    add favicon log

--- _app/immutable/chunks/paths.CvtpRdWe.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_dpg3ti)==null?void 0:s.base)??"/ocs";var t;const e=((t=globalThis.__sveltekit_dpg3ti)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/entry/start.DwCA68xO.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CKf0Mlza.js";export{t as start};
