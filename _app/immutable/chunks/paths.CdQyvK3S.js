c5fff9e3ce4df983dc12d5af4afa60e79b512bf3
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-07-17 17:25:15 +0200

    prerelease test v1.1

--- _app/immutable/chunks/paths.CdQyvK3S.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_d2q2pq)==null?void 0:s.base)??"/ocs";var a;const t=((a=globalThis.__sveltekit_d2q2pq)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.BcXjWN5c.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CKF4o7Ww.js";export{t as start};
