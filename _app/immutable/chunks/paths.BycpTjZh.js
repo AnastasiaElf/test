ca39f5f3a3c1f1c5f73e8db0c322be9849f0384c
AnastasiaElf <anastasia.elf.17@gmail.com>
2024-08-17 22:20:33 +0200

    release test v1.1

---
+++ _app/immutable/chunks/paths.BycpTjZh.js
@@ -0,0 +1,1 @@
+var s;const b=((s=globalThis.__sveltekit_kib7bx)==null?void 0:s.base)??"/ocs";var a;const e=((a=globalThis.__sveltekit_kib7bx)==null?void 0:a.assets)??b;export{e as a,b};
---
+++ _app/immutable/entry/start.4mQvd-XD.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DHC8veWx.js";export{t as start};
